ccb43435247bd87b8caa991362791426cd7ebda6
csj <koreaxman@naver.com>
2021-11-23 04:02:25 -0800

    signal

--- lab3/handlesignal.c
+++ lab3/handlesignal.c
@@ -3,20 +3,21 @@
 #include <unistd.h>
 void handler(int signo);
 
+
 int main()
 {
 	struct sigaction act;
 	int i = 0;
 	act.sa_handler = handler;
 	sigfillset(&(act.sa_mask));
-	sigaction(SIGINT, &act, NULL);
-	printf("SIGNT on\n");
+	sigaction(SIGINT,&act,NULL);
+	printf("SIGINT on\n");
 	while(1){
 		sleep(1);
-		printf("sleep for %d sec(s).\n", ++i);
+		printf("sleep for %d sec(s).\n",++i);
 	}
 }
 void handler(int signo)
 {
-	printf("handler: signo=%d\n", signo);
+	printf("handler: signo=%d\n",signo);
 }
